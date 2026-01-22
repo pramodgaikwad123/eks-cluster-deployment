73293ab60a9aed2cbf07769e11a4de2557bb18e2
Pramod gaikwad <gaikwadgpramod369@gmail.com>
2026-01-22 22:23:46 +0530

    cp

--- main.tf
+++ main.tf
@@ -51,6 +51,6 @@ resource "aws_eks_node_group" "node-grp" {
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
     ]  
 }
